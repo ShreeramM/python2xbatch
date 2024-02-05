3d75219c778790b5c4e85da56c6eab8ca21410eb
ShreeramM <shriram.mehendale@gmail.com>
2024-02-05 08:29:20 +0530

    initial commit

---
+++ 05_Feb_2024/Lab0012.py
@@ -0,0 +1,20 @@
+#Take two numbers from the user and we want to add them
+
+num1 = input("Enter the first Number")
+num2 = input("Enter the Second Number")
+
+print(type(num1))
+
+res = int(num1) + int(num2)
+print(type(res))
+print(res)
+
+num1 = int(input("Enter the first Number"))
+num2 = int(input("Enter the Second Number"))
+
+print(type(num1))
+
+res = num1 + num2
+print(type(res))
+print(res)
+
---
+++ 05_Feb_2024/Lab0014.py
@@ -0,0 +1,7 @@
+#Strings - Bunch of Characters
+# " or ''
+name = "Pramod" #this is also valid
+name = 'Pramod' #this is also valid
+
+str = 'C:/abc/a'
+print(str)
