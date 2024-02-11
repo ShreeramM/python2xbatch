82385df2fca14048bb4bc8dd04677c574daf2b4b
ShreeramM <shriram.mehendale@gmail.com>
2024-02-11 10:18:31 +0530

    07FebClass Assignments

--- 09_Feb_2024/FactorialNumber.py
+++ 09_Feb_2024/FactorialNumber.py
@@ -4,9 +4,11 @@
 # # 5! -->5*4*3*2*1 -> 120
 # # 3! -> 3*2*1 -> 6
 # # 4! -> 4*3*2*1 -> 24
+import math
 
 number = int(input("Enter a number as input: "))
-result = None
-for result in range(1, number):
-    result = result * number
-    print(result)
+# print(math.factorial(number))
+
+for i in range(1, number):
+    number = number * i
+    print("Factorial is: :", number)
